e1723e9e06ab7bd9d34f93160e7be8609abb8b04
andDev777 <ahmedateya47@gmail.com>
2022-08-28 22:18:25 +0300

    first commit

---
+++ app/src/main/java/apps/ateya/cameralocation/MainActivity.kt
@@ -0,0 +1,178 @@
+package apps.ateya.cameralocation
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
+import android.view.View
+import android.widget.*
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.Debug.getLocation
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.common.api.GoogleApi
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var capture_btn: Button? = null
+    private var capturedImg: ImageView? = null
+    private var location_ll: ConstraintLayout? = null
+    private var location_name_tv: TextView? = null
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private var lat: Double? = null
+    private var lng: Double? = null
+
+    companion object {
+        private const val PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 100
+        private const val CAMERA_REQUEST = 1888
+        private const val MY_CAMERA_PERMISSION_CODE = 105
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        capture_btn = findViewById<Button>(R.id.capture_btn)
+        capturedImg = findViewById<ImageView>(R.id.img)
+        location_ll = findViewById<ConstraintLayout>(R.id.location_ll)
+        location_name_tv = findViewById<TextView>(R.id.location_name_tv)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getLocation()
+        capture_btn?.setOnClickListener {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(
+                    arrayOf(Manifest.permission.CAMERA),
+                    MY_CAMERA_PERMISSION_CODE
+                )
+            } else {
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, CAMERA_REQUEST)
+            }
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ), PERMISSION_REQUEST_ACCESS_FINE_LOCATION
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show()
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, CAMERA_REQUEST)
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission", "SetTextI18n")
+    private fun getLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    if (location != null) {
+                        val geocoder = Geocoder(this, Locale.getDefault())
+                        val list: List<Address> =
+                            geocoder.getFromLocation(location.latitude, location.longitude, 1)
+                        lat = list[0].latitude
+                        lng = list[0].longitude
+
+                        location_name_tv?.text = list[0].getAddressLine(0)
+                        println("currentLocationIs === ${list[0].getAddressLine(0)}")
+
+//                        "Country Name\n${list[0].countryName}"
+//                        "Latitude\n${list[0].latitude}"
+//                        "Longitude\n${list[0].longitude}"
+//                        "Locality\n${list[0].locality}"
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            val photo = data?.extras!!["data"] as Bitmap?
+            capturedImg?.setImageBitmap(photo)
+            location_ll?.visibility = View.VISIBLE
+            getLocation()
+        }
+    }
+    private fun getAddress(latitude: Double, longitude: Double): String {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val addresses: List<Address>?
+        val address: Address?
+        var addressText = ""
+
+        addresses = geocoder.getFromLocation(latitude, longitude, 1)
+
+        if (addresses.isNotEmpty()) {
+            address = addresses[0]
+            addressText = address.getAddressLine(0)
+        }
+        return addressText
+    }
+}
